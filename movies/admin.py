d4f80624f24c56afbd42f908974f19a43b2535e2
Vadim <vadimstrd@gmail.com>
2024-08-18 22:27:08 +0700

    Django admin interface fixes. Added forms for adding posts.

--- movies/admin.py
+++ movies/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin, messages
-from .models import Movie, Category, Person, TagPost
+from .models import Movie, Category, Person, TagPost, Role, MoviePersonRole
+from django.utils.safestring import mark_safe
+
+
+# Инлайн для заполнения состава фильма
+class MoviePersonRoleInline(admin.TabularInline):
+    model = MoviePersonRole
+    extra = 1
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "person":
+            kwargs["queryset"] = Person.objects.all()
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class CategoryReverseFilter(admin.SimpleListFilter):
+    title = 'Категория'
+    parameter_name = 'category'
+
+    def lookups(self, request, model_admin):
+        # Категории, отсортированные в обратном порядке
+        categories = Category.objects.all().order_by('-name')
+        return [(category.id, category.name) for category in categories]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(cat__id__exact=self.value())
+        return queryset
 
 
 class GenreFilter(admin.SimpleListFilter):
@@ -7,7 +34,7 @@ class GenreFilter(admin.SimpleListFilter):
     parameter_name = 'genre'
 
     def lookups(self, request, model_admin):
-        genres = TagPost.objects.all()
+        genres = TagPost.objects.all().order_by('tag')
         return [(genre.id, genre.tag) for genre in genres]
 
     def queryset(self, request, queryset):
@@ -16,29 +43,84 @@ class GenreFilter(admin.SimpleListFilter):
         return queryset
 
 
+class RoleFilter(admin.SimpleListFilter):
+    title = 'Роль'
+    parameter_name = 'role'
+
+    def lookups(self, request, model_admin):
+        roles = Role.objects.all().order_by('role')
+        return [(role.id, role.role) for role in roles]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(person_roles__role__id=self.value())
+        return queryset
+
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ('title', 'time_create', 'is_published', 'cat', 'brief_info', 'director_info')
-    list_display_links = ('title',)
-    ordering = ['time_create', 'title']
-    list_editable = ('is_published', 'cat')
-    list_per_page = 10
+    list_display = (
+        'post_poster',
+        'title',
+        'category_display',
+        'genres_display',
+        'release_date_year',
+        'rating',
+        'director_info',
+        'is_published',
+        # 'brief_info',
+    )
+
+    fields = ('title', 'title_en', 'slug', 'cat', 'release_date', 'rating', 'budget',
+              'poster', 'post_poster', 'tags', 'content', 'is_published', 'time_create', 'time_update')
+
+    list_display_links = ('title', )
+    list_editable = ('is_published',)
+    list_per_page = 15
+    list_filter = [CategoryReverseFilter, GenreFilter, 'is_published']
+
+    search_fields = ['title', 'title_en', 'cat__name', 'tags__tag',
+                     'people_roles__name_ru', 'people_roles__name_en']
+    readonly_fields = ['post_poster','time_create', 'time_update']
+    # ordering = ['people_roles__name_ru', 'title']
+    ordering = ['title']
     actions = ['set_published', 'set_draft']
-    search_fields = ['title', 'cat__name']
-    list_filter = ['cat__name', 'is_published', GenreFilter]
+    inlines = [MoviePersonRoleInline]
 
+    save_on_top = True
 
-    @admin.display(description="Краткое описание")
+    @admin.display(description="Изображение")
+    def post_poster(self, movie: Movie):
+        if movie.poster:
+            return mark_safe(f"<img src='{movie.poster.url}' width=50>")
+        return "Без постера"
+
+    @admin.display(description="Категория")
+    def category_display(self, movie: Movie):
+        return movie.cat.name
+
+    @admin.display(description="Жанры")
+    def genres_display(self, movie: Movie):
+        return ", ".join(tag.tag for tag in movie.tags.all())
+
+    @admin.display(description="Год", ordering="release_date")
+    def release_date_year(self, movie: Movie):
+        return movie.release_date.year if movie.release_date else "Не указан"
+
+    @admin.display(description="Описание")
     def brief_info(self, movie: Movie):
         return f"Описание {len(movie.content)} символов."
 
-    @admin.display(description="Режиссер")
+    @admin.display(description="Режиссер", ordering="people_roles__name_ru")
     def director_info(self, movie: Movie):
-        return ", ".join([person.name_ru for person in movie.details.person.all()])
+        # Фильтруем MoviePersonRole по текущему фильму и роли
+        directors = movie.movie_roles.filter(role_id=1)
+        return ", ".join([mpr.person.name_ru for mpr in directors])
 
     @admin.action(description="Опубликовать выбранные записи")
     def set_published(self, request, queryset):
-        queryset.update(is_published=Movie.Status.PUBLISHED)
+        count = queryset.update(is_published=Movie.Status.PUBLISHED)
+        self.message_user(request, f"Изменено {count} записи(ей).")
 
     @admin.action(description="Снять с публикации выбранные записи")
     def set_draft(self, request, queryset):
@@ -55,6 +137,46 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
-    list_display = ('name_ru',)
+    list_display = ('name_ru', 'name_en', 'role_display')
     list_display_links = ('name_ru',)
     ordering = ['name_ru']
+    search_fields = ['name_ru', 'name_en']
+    list_filter = [RoleFilter]
+    exclude = ('m_count', 'role')
+
+    readonly_fields = ('movie_roles_info',)  # Добавляем новое поле в readonly_fields
+
+    def movie_roles_info(self, person: Person):
+        roles = MoviePersonRole.objects.filter(person=person)
+        if not roles.exists():
+            return "Нигде не участвовал"
+        return "\n".join([f"{role.role.role} в {role.movie.title} "
+                          f"({role.movie.release_date.year})" for role in roles])
+
+    movie_roles_info.short_description = "Деятельность"
+
+    @admin.display(description="Деятельность")
+    def role_display(self, person: Person):
+        # Все роли, связанные с человеком через модель MoviePersonRole
+        roles = person.person_roles.values_list('role__role', flat=True).distinct()
+        return ", ".join(roles)
+
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('role',)
+    search_fields = ['role']
+
+    def delete_model(self, request, obj):
+        if MoviePersonRole.objects.filter(role=obj).exists():
+            self.message_user(request, "Невозможно удалить роль, так как она используется в фильмах.",
+                              messages.ERROR)
+            return
+        super().delete_model(request, obj)
+
+
+@admin.register(TagPost)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('tag',)
+    search_fields = ['tag']
+    ordering = ['tag']
--- movies/models.py
+++ movies/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
+from django.core.exceptions import ValidationError
+
+import re
+
+
+def translit_to_eng(s: str) -> str:
+    d = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd',
+         'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'j', 'к': 'k',
+         'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r',
+         'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch',
+         'ш': 'sh', 'щ': 'shch', 'ь': '', 'ы': 'y',
+         'ъ': '', 'э': 'e', 'ю': 'yu', 'я': 'ya'}
+
+    return "".join(map(lambda x: d[x] if d.get(x, False) else x, s.lower()))
 
 
 class PublishedModel(models.Manager):
@@ -16,32 +30,76 @@ class Movie(models.Model):
     objects = models.Manager()
     published = PublishedModel()
 
-    title = models.CharField(max_length=255, verbose_name="Заголовок")
-    slug = models.SlugField(max_length=255, db_index=True, unique=True)
+    # Основные поля
+    title = models.CharField(max_length=255, verbose_name="Заголовок", help_text="Введите название фильма на русском")
+    title_en = models.CharField(blank=True, null=True, max_length=255, verbose_name="Английское название")
+    slug = models.SlugField(null=True, max_length=255, db_index=True, unique=True, blank=True, verbose_name="URL")
     content = models.TextField(blank=True, verbose_name="Описание")
+    poster = models.ImageField(upload_to='posters/', null=True, blank=True, verbose_name="Постер")
+
     time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
     time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
-    is_published = models.BooleanField(choices=tuple(map(lambda x: (bool(x[0]), x[1]), Status.choices)), default=Status.DRAFT, verbose_name="Статус")
-    poster = models.ImageField(upload_to='posters/', null=True, blank=True, verbose_name="Постер")
+    is_published = models.BooleanField(choices=tuple(map(lambda x: (bool(x[0]), x[1]), Status.choices)),
+                                       default=Status.DRAFT, verbose_name="Статус")
+
+    release_date = models.DateField(blank=True, null=True, verbose_name="Дата выхода")
+    budget = models.DecimalField(max_digits=12, decimal_places=2, blank=True, null=True, verbose_name="Бюджет")
+    rating = models.FloatField(null=True, blank=True, verbose_name="Рейтинг")
+
+    # Связанные поля
     cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='list', verbose_name="Категории")
     tags = models.ManyToManyField('TagPost', blank=True, related_name='tags', verbose_name="Жанр")
-    details = models.OneToOneField('MovieDetails', on_delete=models.CASCADE, null=True, blank=True, related_name='movie', verbose_name="Детали")
+    people_roles = models.ManyToManyField('Person', blank=True, through='MoviePersonRole', related_name='movies_roles',
+                                          verbose_name="Участники")  # Участники и их роли
 
     class Meta:
         ordering = ['-time_create']
         indexes = [models.Index(fields=['-time_create'])]
-        verbose_name = 'кино'
+        verbose_name = 'Кино'
         verbose_name_plural = 'Фильмы и сериалы'
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
 
+    def save(self, *args, **kwargs):
+        if not self.title_en:
+            self.title_en = translit_to_eng(self.title)
+
+        if not self.slug:
+            base_slug = slugify(self.title_en)
+
+            # Проверяем, есть ли дата выхода
+            if self.release_date:
+                slug_with_year = f"{base_slug}-{self.release_date.year}"
+            else:
+                slug_with_year = base_slug
+
+            unique_slug = slug_with_year
+            num = 1
+            while Movie.objects.filter(slug=unique_slug).exists():
+                unique_slug = f"{slug_with_year}-{num}"
+                num += 1
+            self.slug = unique_slug
+
+        super().save(*args, **kwargs)
+
+    def clean(self):
+        if self.budget and self.budget < 0:
+            raise ValidationError('Бюджет не может быть отрицательным.')
+
+        if self.title and not re.match(r'^[а-яА-ЯёЁ0-9\s\-\'\":,]+$', self.title):
+            raise ValidationError("Название должно содержать только русские буквы и допустимые символы.")
+
+        if self.title_en and not re.match(r'^[a-zA-Z0-9\s\-\'\":,]+$', self.title_en):
+            raise ValidationError("Название должно содержать только английские буквы и допустимые символы.")
+
     def __str__(self):
         return self.title
 
 
+# Категории (фильмы/сериалы/ и тд)
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True, verbose_name="Категория")
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
 
     class Meta:
@@ -55,9 +113,14 @@ class Category(models.Model):
         return self.name
 
 
+# Жанры
 class TagPost(models.Model):
-    tag = models.CharField(max_length=100, db_index=True)
+    tag = models.CharField(max_length=100, db_index=True, verbose_name='Жанр')
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
 
     def get_absolute_url(self):
         return reverse('tag', kwargs={'cat_slug': Category.slug, 'tag_slug': self.slug})
@@ -66,27 +129,18 @@ class TagPost(models.Model):
         return self.tag
 
 
-class MovieDetails(models.Model):
-    release_date = models.DateField()
-    budget = models.DecimalField(max_digits=12, decimal_places=2)
-    rating = models.FloatField(null=True, blank=True)
-    person = models.ManyToManyField('Person', blank=True, related_name='details')
-
-    def __str__(self):
-        return f"{self.movie.title} ({self.release_date.year})"
-
-
+# Люди
 class Person(models.Model):
     name_ru = models.CharField(max_length=100, verbose_name='Имя на русском')
     name_en = models.CharField(max_length=100, verbose_name='Имя латиницей')
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
     photo = models.ImageField(upload_to='persons/', null=True, blank=True, verbose_name='Фото')
     bio = models.TextField(blank=True, null=True, verbose_name='Информация')
-    role = models.ManyToManyField('Role', blank=True, related_name='person', verbose_name='Деятельность')
+    m_count = models.IntegerField(blank=True, default=0)
 
     class Meta:
-        verbose_name = 'Человек'
-        verbose_name_plural = 'Люди'
+        verbose_name = 'Участник фильма'
+        verbose_name_plural = 'Участники фильмов'
 
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -101,8 +155,35 @@ class Person(models.Model):
 
 
 class Role(models.Model):
-    role = models.CharField(max_length=100, db_index=True)
+    role = models.CharField(max_length=100, db_index=True, verbose_name="Роль")
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    class Meta:
+        verbose_name = 'Роль'
+        verbose_name_plural = 'Роли'
+
+    def delete(self, *args, **kwargs):
+        # Проверяем, есть ли связанные записи в MoviePersonRole
+        if MoviePersonRole.objects.filter(role=self).exists():
+            raise ValidationError("Невозможно удалить роль, так как она используется в связях с фильмами.")
+        super().delete(*args, **kwargs)
 
     def __str__(self):
         return self.role
+
+
+class MoviePersonRole(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='movie_roles')
+    person = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='person_roles')
+    role = models.ForeignKey(Role, on_delete=models.CASCADE, related_name='role_movie')
+
+    class Meta:
+        verbose_name = 'Участник фильма и его роль'
+        verbose_name_plural = 'Участники фильмов и их роли'
+        unique_together = ('movie', 'person', 'role')
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.person.name_ru} - {self.role.role} в {self.movie.title}"
